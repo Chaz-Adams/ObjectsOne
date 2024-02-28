fd50219b2c79e6d249e0646746e452d056980feb
Chaz-Adams <caadams1922@gmail.com>
2024-02-28 14:53:51 -0500

    I added classes and made objects from them

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -6,5 +6,18 @@ public class Main {
 
     public static void main(String... args) {
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+
+        Person person = new Person("Chaz",178);
+        Chair chair = new Chair("wood","brown",4);
+        Dog dog = new Dog("fido",3,"wolf");
+        Car car = new Car("Chevy","Cruze",2021,67483);
+
+        System.out.println(person.height);
+    }
+
+
+
 }
+
+
